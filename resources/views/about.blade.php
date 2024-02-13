2401531d2cc7f179d83a063e55577ea558bd4707
b13nmaN <blenmanjoel@gmail.com>
2024-02-12 22:01:36 -0400

    Styled and populated about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/child.blade.php -->
+ 
+@extends('layouts.app')
+ 
+<!-- @section('title', 'Page Title') -->
+ 
+@section('content') 
+    <h2>Place Your Name Here</h2> 
+    <p>Write a short paragraph about yourself here.</p> 
+    <img src="url('/images/PictureDay.jpg')" alt="Joel Blenman">
+@endsection
